bc9fb1eb282a9bf4b687ebcf442056256208105b
yimmit303 <timsteiner303@gmail.com>
2019-08-25 14:22:18 -0400

    Changes /spawn and /warp
    Will force the player into using /exitmatch first and that command will take them to spawn

--- src/main/java/com/github/yimmit/warfare44/listeners/CommandListener.java
+++ src/main/java/com/github/yimmit/warfare44/listeners/CommandListener.java
@@ -1,16 +1,26 @@
 package com.github.yimmit.warfare44.listeners;
 
+import com.github.yimmit.warfare44.Warfare44;
+import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.cause.EventContextKeys;
 import org.spongepowered.api.event.command.SendCommandEvent;
+import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.format.TextColors;
 
 public class CommandListener
 {
     @Listener
     public void onCommandSend(SendCommandEvent event)
     {
-        if(event.getCommand().compareTo("spawn") == 0)
+        String command = event.getCommand();
+        if(command.compareTo("spawn") == 0 || command.compareTo("warp") == 0)
         {
-            event.setCommand("kill");
+            Player commandplayer = event.getContext().get(EventContextKeys.PLAYER).get();
+            if(Warfare44.getWarfare44().getDeathMatch().isPlayerActive(commandplayer.getUniqueId()))
+            {
+                event.setCommand("exitmatch");
+            }
         }
     }
 }
