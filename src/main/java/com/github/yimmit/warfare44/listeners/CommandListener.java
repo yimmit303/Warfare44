7191fc2ee3634957a485bef1a8dd011176f85542
yimmit303 <timsteiner303@gmail.com>
2019-08-26 22:19:59 -0400

    Fixed the error in CommandLister.java
    Actually gets the player correctly and runs exitmatch on them first

--- src/main/java/com/github/yimmit/warfare44/listeners/CommandListener.java
+++ src/main/java/com/github/yimmit/warfare44/listeners/CommandListener.java
@@ -3,10 +3,7 @@ package com.github.yimmit.warfare44.listeners;
 import com.github.yimmit.warfare44.Warfare44;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Listener;
-import org.spongepowered.api.event.cause.EventContextKeys;
 import org.spongepowered.api.event.command.SendCommandEvent;
-import org.spongepowered.api.text.Text;
-import org.spongepowered.api.text.format.TextColors;
 
 public class CommandListener
 {
@@ -16,7 +13,7 @@ public class CommandListener
         String command = event.getCommand();
         if(command.compareTo("spawn") == 0 || command.compareTo("warp") == 0)
         {
-            Player commandplayer = event.getContext().get(EventContextKeys.PLAYER).get();
+            Player commandplayer = event.getCause().first(Player.class).get();
             if(Warfare44.getWarfare44().getDeathMatch().isPlayerActive(commandplayer.getUniqueId()))
             {
                 event.setCommand("exitmatch");
