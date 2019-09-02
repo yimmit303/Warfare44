9e130d63db46b69c7d0b629cb93a8233cbd97c6e
yimmit303 <timsteiner303@gmail.com>
2019-09-01 20:52:11 -0400

    Added DeathmathcUtil.java for useful functions

---
+++ src/main/java/com/github/yimmit/warfare44/util/DeathmatchUtil.java
@@ -0,0 +1,18 @@
+package com.github.yimmit.warfare44.util;
+
+import com.github.yimmit.warfare44.Warfare44;
+import org.spongepowered.api.entity.living.player.Player;
+
+import java.util.UUID;
+
+public class DeathmatchUtil
+{
+    public static void clearInventory(UUID id)
+    {
+        if(Warfare44.getWarfare44().getGame().getServer().getPlayer(id).isPresent())
+        {
+            Player player = Warfare44.getWarfare44().getGame().getServer().getPlayer(id).get();
+            player.getInventory().clear();
+        }
+    }
+}
