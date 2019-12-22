2f7a56784967cc26a0216c2c2b3ef96e17ab18b6
yimmit303 <timsteiner303@gmail.com>
2019-12-21 21:30:30 -0500

    New and updated functions

--- src/main/java/com/github/yimmit/warfare44/util/InventoryUtil.java
+++ src/main/java/com/github/yimmit/warfare44/util/InventoryUtil.java
@@ -1,11 +1,13 @@
 package com.github.yimmit.warfare44.util;
 
 import com.github.yimmit.warfare44.Warfare44;
+import com.github.yimmit.warfare44.model.TDMClass;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.item.ItemType;
 import org.spongepowered.api.item.ItemTypes;
 import org.spongepowered.api.item.inventory.ItemStack;
 import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.format.TextColors;
 
 import java.util.UUID;
 
@@ -20,21 +22,37 @@ public class InventoryUtil
         }
     }
 
-    public static void giveItem(UUID id)
+    public static void giveItem(UUID id, String itemname, int number)
     {
         if(Warfare44.getWarfare44().getGame().getServer().getPlayer(id).isPresent())
         {
             Player player = Warfare44.getWarfare44().getGame().getServer().getPlayer(id).get();
-            ItemStack gun;
-            String typeid = "flansmod:ak47";
-            if(Warfare44.getWarfare44().getGame().getRegistry().getType(ItemType.class, typeid).isPresent())
+            if(Warfare44.getWarfare44().getGame().getRegistry().getType(ItemType.class, itemname).isPresent())
             {
-                player.sendMessage(Text.of("We found item of name " + typeid));
-                gun = ItemStack.builder().itemType(Warfare44.getWarfare44().getGame().getRegistry().getType(ItemType.class, typeid).get()).build();
-                player.getInventory().offer(gun);
-                return;
+                ItemStack item = ItemStack.builder().itemType(Warfare44.getWarfare44().getGame().getRegistry().getType(ItemType.class, itemname).get()).quantity(number).build();
+                player.getInventory().offer(item);
             }
-            player.sendMessage(Text.of("We did not find item of name " + typeid));
+            else
+            {
+                player.sendMessage(Text.of(TextColors.RED, "Error getting item of name " + itemname + " please report this error to staff or the discord"));
+            }
+        }
+    }
+
+    public static void giveMaxItem(UUID id, String itemname)
+    {
+        if(Warfare44.getWarfare44().getGame().getServer().getPlayer(id).isPresent())
+        {
+            Player player = Warfare44.getWarfare44().getGame().getServer().getPlayer(id).get();
+            if (Warfare44.getWarfare44().getGame().getRegistry().getType(ItemType.class, itemname).isPresent())
+            {
+                ItemType item = Warfare44.getWarfare44().getGame().getRegistry().getType(ItemType.class, itemname).get();
+                giveItem(id, itemname, item.getMaxStackQuantity());
+            }
+            else
+            {
+                player.sendMessage(Text.of(TextColors.RED, "Error getting item of name " + itemname + " please report this error to staff or the discord"));
+            }
         }
     }
 }
