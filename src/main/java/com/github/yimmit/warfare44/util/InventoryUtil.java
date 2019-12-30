5b084745aeaca0fc5a99c3318b6452779d5b3b3f
yimmit303 <timsteiner303@gmail.com>
2019-12-29 21:18:05 -0500

    Made the give armor functions

--- src/main/java/com/github/yimmit/warfare44/util/InventoryUtil.java
+++ src/main/java/com/github/yimmit/warfare44/util/InventoryUtil.java
@@ -1,10 +1,8 @@
 package com.github.yimmit.warfare44.util;
 
 import com.github.yimmit.warfare44.Warfare44;
-import com.github.yimmit.warfare44.model.TDMClass;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.item.ItemType;
-import org.spongepowered.api.item.ItemTypes;
 import org.spongepowered.api.item.inventory.ItemStack;
 import org.spongepowered.api.text.Text;
 import org.spongepowered.api.text.format.TextColors;
@@ -48,6 +46,74 @@ public class InventoryUtil
             {
                 ItemType item = Warfare44.getWarfare44().getGame().getRegistry().getType(ItemType.class, itemname).get();
                 giveItem(id, itemname, item.getMaxStackQuantity());
+            }
+            else
+            {
+                player.sendMessage(Text.of(TextColors.RED, "Error getting item of name " + itemname + " please report this error to staff or the discord"));
+            }
+        }
+    }
+
+    public static void giveHead(UUID id, String itemname)
+    {
+        if(Warfare44.getWarfare44().getGame().getServer().getPlayer(id).isPresent())
+        {
+            Player player = Warfare44.getWarfare44().getGame().getServer().getPlayer(id).get();
+            if (Warfare44.getWarfare44().getGame().getRegistry().getType(ItemType.class, itemname).isPresent())
+            {
+                ItemType item = Warfare44.getWarfare44().getGame().getRegistry().getType(ItemType.class, itemname).get();
+                player.setHelmet(ItemStack.of(item));
+            }
+            else
+            {
+                player.sendMessage(Text.of(TextColors.RED, "Error getting item of name " + itemname + " please report this error to staff or the discord"));
+            }
+        }
+    }
+
+    public static void giveChest(UUID id, String itemname)
+    {
+        if(Warfare44.getWarfare44().getGame().getServer().getPlayer(id).isPresent())
+        {
+            Player player = Warfare44.getWarfare44().getGame().getServer().getPlayer(id).get();
+            if (Warfare44.getWarfare44().getGame().getRegistry().getType(ItemType.class, itemname).isPresent())
+            {
+                ItemType item = Warfare44.getWarfare44().getGame().getRegistry().getType(ItemType.class, itemname).get();
+                player.setChestplate(ItemStack.of(item));
+            }
+            else
+            {
+                player.sendMessage(Text.of(TextColors.RED, "Error getting item of name " + itemname + " please report this error to staff or the discord"));
+            }
+        }
+    }
+
+    public static void giveLegs(UUID id, String itemname)
+    {
+        if(Warfare44.getWarfare44().getGame().getServer().getPlayer(id).isPresent())
+        {
+            Player player = Warfare44.getWarfare44().getGame().getServer().getPlayer(id).get();
+            if (Warfare44.getWarfare44().getGame().getRegistry().getType(ItemType.class, itemname).isPresent())
+            {
+                ItemType item = Warfare44.getWarfare44().getGame().getRegistry().getType(ItemType.class, itemname).get();
+                player.setLeggings(ItemStack.of(item));
+            }
+            else
+            {
+                player.sendMessage(Text.of(TextColors.RED, "Error getting item of name " + itemname + " please report this error to staff or the discord"));
+            }
+        }
+    }
+
+    public static void giveBoots(UUID id, String itemname)
+    {
+        if(Warfare44.getWarfare44().getGame().getServer().getPlayer(id).isPresent())
+        {
+            Player player = Warfare44.getWarfare44().getGame().getServer().getPlayer(id).get();
+            if (Warfare44.getWarfare44().getGame().getRegistry().getType(ItemType.class, itemname).isPresent())
+            {
+                ItemType item = Warfare44.getWarfare44().getGame().getRegistry().getType(ItemType.class, itemname).get();
+                player.setBoots(ItemStack.of(item));
             }
             else
             {
